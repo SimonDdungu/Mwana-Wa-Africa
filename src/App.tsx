d435777a931bd440e64c919fed0d5d0bd45b4d4f
Simon Chainbers <schainbers@gmail.com>
2024-11-01 16:50:01 +0300

    Trying to push react app from VS code terminal

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import NavBAR from './NavBar'
+import HeroSection from './HeroSection'
+import VolunteerBanner from './VolunteerBanner'
+import Feautres from './Features'
+
+
+
+
+//CSS STYLES
+import './Navigation.css'
+import './HeroSection.css'
+import './VolunteerBanner.css'
+import './Features.css'
+
+
+
+//Main Page
+
+function App() {
+    return <>
+            <NavBAR />
+            <HeroSection />
+            <VolunteerBanner />
+            <Feautres />
+           </>
+}
+
+export default App
