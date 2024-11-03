850b856ae459b5fa15ed257b22c4cf06670436cd
Simon Chainbers <schainbers@gmail.com>
2024-11-03 19:18:17 +0300

    Testing if the problem was with the first mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Volunteer from "./components/Volunteer";
+import Feautres from "./components/Features";
+import Causes from "./components/Causes";
 
 const App = () => {
   return(
       <>
         <Header />
         <Hero />
+        <Volunteer />
+        <Feautres />
+        <Causes />
       </>
   );
 };
