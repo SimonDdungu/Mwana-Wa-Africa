84dad639a7905c6b86d5cc4162fc1d29b53e191e
Simon Chainbers <schainbers@gmail.com>
2024-11-03 16:56:26 +0300

    First Section in React test

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+            <header className="Navigation_Bar">
+                    <h2>Mwana Wa Africa</h2>
+                        <nav>
+                            <ul className="Nav_items">
+                                <li className="Nav_links active  collapse_on_mobile"><a href="#">Home</a></li>
+                                <li className="Nav_links  collapse_on_mobile"><a href="#">About Us</a></li>
+                                <li className="Nav_links  collapse_on_mobile"><a href="#">Causes</a></li>
+                                <li className="Nav_links  collapse_on_mobile"><a href="#">Blogs</a></li>
+                                <li className="Nav_links  collapse_on_mobile"><a href="#">Contact Us</a></li>
+                            </ul>
+                        </nav>
+
+                    <a className="Donate_Navbar collapse_on_mobile" href="#">Donate Now</a>
+
+
+                    <div className="Hamburger_menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+
+            </header>
+    );
+};
+
+export default Header;
